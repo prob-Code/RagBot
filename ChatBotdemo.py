64f68a3677bc043972b87c4e2e0f7fb10ee5014f
Ojas Satdeve <ojassatdeve@gamil.com>
2026-02-21 15:17:50 +0530

    Add RAG Chatbot files: ChatBotdemo.py, C++_Introduction.txt, requirements.txt, .gitignore

---
+++ ChatBotdemo.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+
+st.set_page_config(page_title="C++ RAG Chatbot")
+st.title("C++ RAG Chatbot")
+st.write("Upload your C++ document and ask questions about it")
+
+@st.cache_resource
+def load_vectorstore():
+    loader = TextLoader("c++_introduction.txt", encoding="utf-8")
+    data = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=200,
+        chunk_overlap=20
+    )
+
+    final_documents = text_splitter.split_documents(documents=data)
+
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+    db = FAISS.from_documents(final_documents, embeddings)
+    return db
+
+db = load_vectorstore()
+
+query = st.text_input("Ask a question about C++:")
+
+if query:
+    docs = db.similarity_search(query, k=3)
+    st.subheader("Retrieved Context")
+    for i, doc in enumerate(docs):
+        st.markdown(f"**Result {i+1}**")
+        st.write(doc.page_content)
